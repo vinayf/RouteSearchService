a7c879fb6f1e4073c781b1c51f096a66600c13be
vfulari <vinay_fulari@mcafee.com>
2017-09-04 13:29:11 +0530

    Added empty request param check.

--- src/main/java/com/ge/route/search/service/RouteSearchEndpoint.java
+++ src/main/java/com/ge/route/search/service/RouteSearchEndpoint.java
@@ -1,6 +1,8 @@
 package com.ge.route.search.service;
 
 import com.ge.route.search.cache.StationGraphCache;
+import com.ge.route.search.exceptions.ErrorCodes;
+import com.ge.route.search.exceptions.RouteSearchException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +28,8 @@ public class RouteSearchEndpoint {
                                     @RequestParam (value="dep_sid", required = true) Integer departureStationId,
                                     @RequestParam(value="arr_sid", required = true) Integer arrivalStationId){
 
+        if(null == departureStationId || null == arrivalStationId)
+            throw new RouteSearchException(ErrorCodes.MANDATORY_QUERY_PARAM_REQUIRED_NON_NULL.getValue());
         LOGGER.debug("Searching for route between "+departureStationId+" & "+arrivalStationId);
         boolean isConnected = stationNetworkCache.isDirectlyConnected(departureStationId,arrivalStationId);
         return new RouteSearchResponse(departureStationId,arrivalStationId,isConnected);
